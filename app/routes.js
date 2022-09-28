5d99d35b18b4b664ef7c178673d9739c78ac93e7
KarimL <karim.latchman@kainos.com>
2022-09-28 14:53:08 +0100

    UI for 003

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const interface = require('./interface.js')
+const capability = require('./interface.js')
 
 // Add your routes here - above the module.exports line
+router.get('/view-capabilities', async (req, res) => {
+    var result = await capability.getCapbilities()
+    res.render('view-capabilities', {
+    capabilities: result
+    })});
 
 module.exports = router
