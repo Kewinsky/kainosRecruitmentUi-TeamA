97db6f300b33c57a0763d592d60d531c6edfb53b
KarimL <karim.latchman@kainos.com>
2022-09-29 15:55:22 +0100

    routes.js changes to suit merge

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const interface = require('./interface.js')
+
 const capability = require('./interface.js')
 
 // Add your routes here - above the module.exports line
