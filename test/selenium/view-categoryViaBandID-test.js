c6ef74685ce646268e77628023cfb27d30fe179b
Danny Che <danny.che@kainos.com>
2022-10-10 14:12:40 +0100

    added selenium test to userstory 9

---
+++ test/selenium/view-categoryViaBandID-test.js
@@ -0,0 +1,31 @@
+var chai = require('chai');
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+const By = webdriver.By
+let service = new chrome.ServiceBuilder()
+        .loggingTo('/my/log/file.txt')
+        .enableVerboseLogging()
+        .build();
+
+const { assert } = require('chai');
+const expect = chai.expect;
+var baseUrl = 'http://localhost:3000';
+var bandInfoTrainingUrl = baseUrl.concat('/view-band-info/1');
+
+describe('UI Testing for category associated with training via bandID', () => {
+  it('Should display a table with data containing a category column with its respectable data given the bandID', async() => {
+       var driver = new webdriver.Builder()
+           .withCapabilities(webdriver.Capabilities.chrome())
+           .build();
+
+       // Open specify webpage
+       driver.get(bandInfoTrainingUrl);
+
+       // Compare expected category name with actual category name for bandID 1
+       expect(await driver.findElement(By.id('CategoryID-2022-11-17')).getText()).to.equal('Professional skills');
+
+       // Close browser
+       await driver.quit();
+  })
+})
